b29ed261855d73885817a33f268bafa7e6d3b4b3
TUANKIET0397 <kiet8103@gmail.com>
2026-05-20 23:40:34 +0700

    feat: implement local authentication strategy with Passport.js and update auth module

--- nest-app/src/auth/auth.service.ts
+++ nest-app/src/auth/auth.service.ts
@@ -10,9 +10,8 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
-
     if (!user) {
       throw new UnauthorizedException('Username is incorrect');
     }
@@ -21,10 +20,35 @@ export class AuthService {
     if (!isValidPassword) {
       throw new UnauthorizedException('password is incorrect');
     }
+    return user;
+  }
 
-    const payload = { sub: user._id, username: user.email };
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: this.jwtService.sign(payload),
     };
   }
+
+  // async signIn(username: string, pass: string): Promise<any> {
+  //   const user = await this.usersService.findByEmail(username);
+
+  //   if (!user) {
+  //     throw new UnauthorizedException('Username is incorrect');
+  //   }
+
+  //   const isValidPassword = await comparePasswordHelper(pass, user.password);
+  //   if (!isValidPassword) {
+  //     throw new UnauthorizedException('password is incorrect');
+  //   }
+
+  //   // if (!user) return null;
+
+  //   // return user;
+
+  //   // const payload = { sub: user._id, username: user.email };
+  //   // return {
+  //   //   access_token: await this.jwtService.signAsync(payload),
+  //   // };
+  // }
 }
