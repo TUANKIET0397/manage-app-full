ea06d27d10933ac5117672e180189109b78f1540
TUANKIET <146311369+TUANKIET0397@users.noreply.github.com>
2026-08-20 00:28:04 +0700

    fix: update package-lock.json to use exact versions for dependencies and devDependencies; refactor ManageUserPage for improved readability and consistency

--- next-app/src/app/(admin)/dashboard/user/page.tsx
+++ next-app/src/app/(admin)/dashboard/user/page.tsx
@@ -1,40 +1,41 @@
-import { auth } from "@/auth"
-import UserTable from "@/components/admin/user.table"
-import { sendRequest } from "@/utils/api"
+import { auth } from "@/auth";
+import UserTable from "@/components/admin/user.table";
+import { sendRequest } from "@/utils/api";
 
 interface IProps {
-    params: { id: string }
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  params: { id: string };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 const ManageUserPage = async (props: IProps) => {
-    const searchParams = await props.searchParams
-    const current = searchParams?.current ?? 1
-    const pageSize = searchParams?.pageSize ?? 10
-    const session = await auth()
+  const searchParams = await props.searchParams;
+  const current = searchParams?.current ?? 1;
+  const pageSize = searchParams?.pageSize ?? 10;
+  const session = await auth();
+  const accessToken = (session?.user as { access_token?: string } | undefined)
+    ?.access_token;
 
-    const res = await sendRequest<IBackendRes<any>>({
-        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users`,
-        method: "GET",
-        queryParams: {
-            current,
-            pageSize,
-        },
-        headers: {
-            Authorization: `Bearer ${session?.user?.access_token}`,
-        },
-        nextOption: {
-            next: { tags: ["list-users"] },
-        },
-    })
+  const res = await sendRequest<IBackendRes<any>>({
+    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users`,
+    method: "GET",
+    queryParams: {
+      current,
+      pageSize,
+    },
+    headers: accessToken
+      ? {
+          Authorization: `Bearer ${accessToken}`,
+        }
+      : {},
+    nextOption: {
+      next: { tags: ["list-users"] },
+    },
+  });
 
-    return (
-        <div>
-            <UserTable
-                users={res?.data?.results ?? []}
-                meta={res?.data?.meta}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <UserTable users={res?.data?.results ?? []} meta={res?.data?.meta} />
+    </div>
+  );
+};
 
-export default ManageUserPage
+export default ManageUserPage;
